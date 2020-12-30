c6d44c04f8d9877a3c539427f08201d085680f62
Josh Allen <josh.allen@pensco.com>
2020-12-30 14:42:21 -0600

    advent of code - day 1 - better solution capable of tuples of any length

--- src/exercises/advent-of-code-2020/day-1-report-repair/sum-finder.js
+++ src/exercises/advent-of-code-2020/day-1-report-repair/sum-finder.js
@@ -1,15 +1,29 @@
 /**
- * Finds two numbers in the given array that sum to the given value.
+ * Finds a tuple from the numbers in the given array that sum to the given value.
  * @param {number[]} numbers
+ * @param {number} tupleSize
  * @param {number} sum
- * @return {null|number[]} An array of two numbers if a result was found, or null if not.
+ * @return {null|number[]} An array of numbers with a length of the tuple size if a result was found, or null if not.
  */
-function findTwoTupleWithSum(numbers, sum) {
-    for (let i = 0; i < numbers.length - 1; i++) {
-        for (let j = 1; j < numbers.length; j++) {
-            if (numbers[i] + numbers[j] === sum) {
-                return [numbers[i], numbers[j]];
-            }
+function findTupleWithSum(numbers, tupleSize, sum) {
+    // invalid case
+    if (tupleSize <= 0) {
+        throw new Error('invalid tuple size');
+    }
+    // the base case: look for a single number that matches the sum
+    if (tupleSize === 1) {
+        const found = numbers.find((i) => i === sum);
+        return found ? [found] : null;
+    }
+
+    // divide into subproblems: look for a tuple that adds up to the sum less the "current" number
+    for (let i = 0; i < numbers.length; i++) {
+        const current = numbers[i];
+        const numbersWithoutCurrent = numbers.slice(0, i).concat(numbers.slice(i + 1));
+        const subtuple = findTupleWithSum(numbersWithoutCurrent, tupleSize - 1, sum - current);
+        if (subtuple) {
+            subtuple.push(current);
+            return subtuple;
         }
     }
 
@@ -17,5 +31,5 @@ function findTwoTupleWithSum(numbers, sum) {
 }
 
 module.exports = {
-    findTwoTupleWithSum,
+    findTupleWithSum,
 };
--- test/exercises/advent-of-code-2020/day-1-report-repair/day-1.spec.js
+++ test/exercises/advent-of-code-2020/day-1-report-repair/day-1.spec.js
@@ -2,11 +2,21 @@ const fs = require('fs');
 const sumFinder = require('../../../../src/exercises/advent-of-code-2020/day-1-report-repair/sum-finder');
 
 describe('Advent of Code 2020 - Day 1', () => {
+   const inputFile =  './test/exercises/advent-of-code-2020/day-1-report-repair/input.txt';
+
    it('should find the two numbers whose sum is 2020 and multiply them', async () => {
-       const numbers = await fs.readFileSync('./test/exercises/advent-of-code-2020/day-1-report-repair/input.txt', 'utf-8').split('\n').map((line) => Number(line));
+       const numbers = await fs.readFileSync(inputFile, 'utf-8').split('\n').map((line) => Number(line));
        const start = new Date();
-       const result = sumFinder.findTwoTupleWithSum(numbers, 2020);
+       const result = sumFinder.findTupleWithSum(numbers, 2, 2020);
        expect(result).to.not.be.null;
-       console.log(`${result[0]} * ${result[1]} = ${result[0] * result[1]}: ${new Date() - start} ms`);
+       console.log(`${result.join(' * ')} = ${result.reduce((a, c) => a * c, 1)}: ${new Date() - start} ms`);
    });
+
+    it('should find the three numbers whose sum is 2020 and multiply them', async () => {
+        const numbers = await fs.readFileSync(inputFile, 'utf-8').split('\n').map((line) => Number(line));
+        const start = new Date();
+        const result = sumFinder.findTupleWithSum(numbers, 3, 2020);
+        expect(result).to.not.be.null;
+        console.log(`${result.join(' * ')} = ${result.reduce((a, c) => a * c, 1)}: ${new Date() - start} ms`);
+    });
 });
