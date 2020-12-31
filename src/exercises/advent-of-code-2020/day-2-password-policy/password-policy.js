72e8d6f948433271ae058b9913b4a536f5c43741
Josh Allen <josh.allen@pensco.com>
2020-12-31 17:59:23 -0600

    advent of code - day 2a - password policy with positional requirements

--- src/exercises/advent-of-code-2020/day-2-password-policy/password-policy.js
+++ src/exercises/advent-of-code-2020/day-2-password-policy/password-policy.js
@@ -1,4 +1,4 @@
-class PasswordPolicy {
+class PasswordPolicyLetterFrequency {
 
     constructor(letter, minCount, maxCount) {
         this.letter = letter;
@@ -7,7 +7,6 @@ class PasswordPolicy {
     }
 
     /**
-     *
      * @param str
      * @return {boolean} true if the given password meets the password policy, false otherwise
      */
@@ -28,11 +27,39 @@ class PasswordPolicy {
     }
 }
 
-const passwordRegex = /(\d+)-(\d+) ([a-z]{1}): (.*)/
+class PasswordPolicyPositional {
 
-function checkPassword(line) {
-    const matches = line.match(passwordRegex);
-    return new PasswordPolicy(matches[3], Number(matches[1]), Number(matches[2])).check(matches[4]);
+    constructor(letter, firstPosition, secondPosition) {
+        this.letter = letter;
+        this.firstPosition = firstPosition;
+        this.secondPosition = secondPosition;
+    }
+
+    /**
+     *
+     * @param str
+     * @return {boolean} true if the given password meets the password policy, false otherwise
+     */
+    check(str) {
+        const firstLetterMatches = str[this.firstPosition - 1] === this.letter;
+        const secondLetterMatches = str[this.secondPosition - 1] === this.letter;
+        return (firstLetterMatches && !secondLetterMatches) || (!firstLetterMatches && secondLetterMatches)
+    }
 }
 
-module.exports = checkPassword;
+const passwordRegex = /(\d+)-(\d+) ([a-z]{1}): (.*)/
+
+function checkPasswordLetterFrequency(line) {
+    const matches = line.match(passwordRegex);
+    return new PasswordPolicyLetterFrequency(matches[3], Number(matches[1]), Number(matches[2])).check(matches[4]);
+}
+
+function checkPasswordPositional(line) {
+    const matches = line.match(passwordRegex);
+    return new PasswordPolicyPositional(matches[3], Number(matches[1]), Number(matches[2])).check(matches[4]);
+}
+
+module.exports = {
+    checkPasswordLetterFrequency,
+    checkPasswordPositional
+};
--- test/exercises/advent-of-code-2020/day-2-password-policy/password-policy.spec.js
+++ test/exercises/advent-of-code-2020/day-2-password-policy/password-policy.spec.js
@@ -1,38 +1,70 @@
 const fs = require('fs');
-const checkPassword = require('../../../../src/exercises/advent-of-code-2020/day-2-password-policy/password-policy');
+const passwordPolicies = require('../../../../src/exercises/advent-of-code-2020/day-2-password-policy/password-policy');
 
 describe('Advent of Code 2020 - Day 2', () => {
     const inputFile =  './test/exercises/advent-of-code-2020/day-2-password-policy/input.txt';
 
-    it('should count the passwords that meet the policy', async () => {
-        const lines = await fs.readFileSync(inputFile, 'utf-8').split('\n').filter((line) => line.length > 0);
-        const linesPassing = lines.filter(checkPassword);
-        console.log(`${linesPassing.length} / ${lines.length} pass the password policy`);
-    });
+    describe('password policy letter frequency', () => {
+        it('should count the passwords that meet the policy', async () => {
+            const lines = await fs.readFileSync(inputFile, 'utf-8').split('\n').filter((line) => line.length > 0);
+            const linesPassing = lines.filter(passwordPolicies.checkPasswordLetterFrequency);
+            console.log(`${linesPassing.length} / ${lines.length} pass the password policy`);
+        });
 
-    it('should identify passwords that do meet the given policy', async () => {
-        const goodPasswords = [
-            '3-9 c: ccc',
-            '3-9 c: ccccccccc',
-            '3-9 c: cacacacacacacacac',
-            '3-9 c: acacacacacacacacaca',
-        ];
-        goodPasswords.forEach((badPassword) => {
-           expect(checkPassword(badPassword)).to.equal(true);
+        it('should identify passwords that do meet the given policy', async () => {
+            const goodPasswords = [
+                '3-9 c: ccc',
+                '3-9 c: ccccccccc',
+                '3-9 c: cacacacacacacacac',
+                '3-9 c: acacacacacacacacaca',
+            ];
+            goodPasswords.forEach((password) => {
+                expect(passwordPolicies.checkPasswordPositional(password)).to.equal(true);
+            });
+        });
+
+        it('should identify passwords that do NOT meet the given policy', async () => {
+            const badPasswords = [
+                '3-9 c: ccaaaaa',
+                '3-9 c: aaaaacc',
+                '3-9 c: aaaccaaa',
+                '3-9 c: cccccccccc',
+                '3-9 c: cacacacacacacacacac',
+                '3-9 c: bbb',
+            ];
+            badPasswords.forEach((password) => {
+                expect(passwordPolicies.checkPasswordPositional(password)).to.equal(false);
+            });
         });
     });
 
-    it('should identify passwords that do NOT meet the given policy', async () => {
-        const badPasswords = [
-            '3-9 c: ccaaaaa',
-            '3-9 c: aaaaacc',
-            '3-9 c: aaaccaaa',
-            '3-9 c: cccccccccc',
-            '3-9 c: cacacacacacacacacac',
-            '3-9 c: bbb',
-        ];
-        badPasswords.forEach((badPassword) => {
-            expect(checkPassword(badPassword)).to.equal(false);
+    describe('password policy positional', () => {
+        it('should count the passwords that meet the policy', async () => {
+            const lines = await fs.readFileSync(inputFile, 'utf-8').split('\n').filter((line) => line.length > 0);
+            const linesPassing = lines.filter(passwordPolicies.checkPasswordPositional);
+            console.log(`${linesPassing.length} / ${lines.length} pass the password policy`);
+        });
+
+        it('should identify passwords that do meet the given policy', async () => {
+            const goodPasswords = [
+                '3-9 c: aacaaaaaa',
+                '3-9 c: aaaaaaaac',
+            ];
+            goodPasswords.forEach((password) => {
+                expect(passwordPolicies.checkPasswordPositional(password)).to.equal(true);
+            });
+        });
+
+        it('should identify passwords that do NOT meet the given policy', async () => {
+            const goodPasswords = [
+                '3-9 c: aaaaaaaaa',
+                '3-9 c: aacaaaaac',
+                '3-9 c: aaacaaaaaa',
+                '3-9 c: aaaaaaaaac',
+            ];
+            goodPasswords.forEach((password) => {
+                expect(passwordPolicies.checkPasswordPositional(password)).to.equal(false);
+            });
         });
     });
 });
