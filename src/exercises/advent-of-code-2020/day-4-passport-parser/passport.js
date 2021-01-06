d98647911d3aa9f8b4df09ee7847171d7e578acf
Josh Allen <josh.allen@pensco.com>
2021-01-06 16:43:25 -0700

    advent of code - day 4b - add more rules to passport validator

--- src/exercises/advent-of-code-2020/day-4-passport-parser/passport.js
+++ src/exercises/advent-of-code-2020/day-4-passport-parser/passport.js
@@ -1,12 +1,35 @@
+const heightValidator = (v) => {
+    if (!v) {
+        return false;
+    }
+
+    const matches = v.match(/(\d+)(cm|in)/);
+    if (matches === null) {
+        return false;
+    }
+
+    const num = matches[1];
+    const unit = matches[2];
+
+    if (unit === 'in') {
+        return num >= 59 && num <= 76;
+    }
+    if (unit === 'cm') {
+        return num >= 150 && num <= 193;
+    }
+
+    return false;
+};
+
 const passportFields = [
-    { name: 'birthYear', required: true },
-    { name: 'issueYear', required: true },
-    { name: 'expirationYear', required: true },
-    { name: 'height', required: true },
-    { name: 'hairColor', required: true },
-    { name: 'eyeColor', required: true },
-    { name: 'passportId', required: true },
-    { name: 'countryId', required: false },
+    { name: 'birthYear', validate: (v) => !!v && v.length === 4 && Number(v) >= 1920 && Number(v) <= 2002 },
+    { name: 'issueYear', validate: (v) => !!v && v.length === 4 && Number(v) >= 2010 && Number(v) <= 2020 },
+    { name: 'expirationYear', validate: (v) => !!v && v.length === 4 && Number(v) >= 2020 && Number(v) <= 2030 },
+    { name: 'height', validate: heightValidator },
+    { name: 'hairColor', validate: (v) => !!v && v.match(/\#[0-9a-z]{6}/) !== null },
+    { name: 'eyeColor', validate: (v) => !!v && ['amb', 'blu', 'brn', 'gry', 'grn', 'hzl', 'oth'].find((color) => color === v) !== undefined },
+    { name: 'passportId', validate: (v) => !!v && v.length === 9 && v.match(/[0-9]{9}/) !== null },
+    { name: 'countryId' },
 ];
 
 class Passport {
@@ -39,9 +62,10 @@ class Passport {
      * @return {boolean} true if the current object is valid, i.e., has values for all required fields; returns false otherwise
      */
     validate() {
-        return passportFields.filter((field) => field.required)
+        return passportFields.filter((field) => typeof field.validate === 'function')
             .reduce((accumulator, currentField) => {
-                return accumulator && !!this[currentField.name]
+                const valid = currentField.validate(this[currentField.name]);
+                return accumulator && valid;
             }, true);
     }
 }
--- test/exercises/advent-of-code-2020/day-4-passport-parser/passport-parser.spec.js
+++ test/exercises/advent-of-code-2020/day-4-passport-parser/passport-parser.spec.js
@@ -19,6 +19,42 @@ describe('passport parser', () => {
        expect(passports[2].validate()).to.equal(true);
    });
 
+   it('should validate eye color', () => {
+       // a valid passport, along with copies with variations on eye color
+       const text = 'ecl:gry pid:860033327 eyr:2020 hcl:#fffffd\n' +
+           'byr:1937 iyr:2017 cid:147 hgt:183cm\n' +
+           '\n' +
+           'ecl:abc pid:860033327 eyr:2020 hcl:#fffffd\n' +
+           'byr:1937 iyr:2017 cid:147 hgt:183cm\n' +
+           '\n' +
+           'pid:860033327 eyr:2020 hcl:#fffffd\n' +
+           'byr:1937 iyr:2017 cid:147 hgt:183cm\n';
+       const passports = passportParser.parseBlob(text);
+       expect(passports[0].validate()).to.equal(true);
+       expect(passports[1].validate()).to.equal(false);
+       expect(passports[2].validate()).to.equal(false);
+   });
+
+    it('should validate passport id', () => {
+        // a valid passport, along with copies with variations on pasport id
+        const text = 'ecl:gry pid:860033327 eyr:2020 hcl:#fffffd\n' +
+            'byr:1937 iyr:2017 cid:147 hgt:183cm\n' +
+            '\n' +
+            'ecl:gry pid:8600333278 eyr:2020 hcl:#fffffd\n' +
+            'byr:1937 iyr:2017 cid:147 hgt:183cm\n' +
+            '\n' +
+            'ecl:gry pid:86003332 eyr:2020 hcl:#fffffd\n' +
+            'byr:1937 iyr:2017 cid:147 hgt:183cm\n' +
+            '\n' +
+            'ecl:gry pid:86003332a eyr:2020 hcl:#fffffd\n' +
+            'byr:1937 iyr:2017 cid:147 hgt:183cm\n';
+        const passports = passportParser.parseBlob(text);
+        expect(passports[0].validate()).to.equal(true);
+        expect(passports[1].validate()).to.equal(false);
+        expect(passports[2].validate()).to.equal(false);
+        expect(passports[3].validate()).to.equal(false);
+    });
+
    it('should parse and validate the input file describing passports and count the valid ones', async () => {
        const inputFile = './test/exercises/advent-of-code-2020/day-4-passport-parser/input.txt';
        const text = await fs.readFileSync(inputFile, 'utf-8');
