c5d63c9b242e30f8c96f547c5c2c3db0d41fc70b
Josh Allen <josh.allen@pensco.com>
2021-01-07 16:01:36 -0700

    advent of code - day 5b - find empty seat in contiguous binary boarding passes

--- test/exercises/advent-of-code-2020/day-5-binary-boarding-pass/binary-boarding-pass.spec.js
+++ test/exercises/advent-of-code-2020/day-5-binary-boarding-pass/binary-boarding-pass.spec.js
@@ -3,17 +3,45 @@ const BinaryBoardingPass = require('../../../../src/exercises/advent-of-code-202
 
 describe('binary boarding pass', () => {
     it('should parse a string representation of the seat and calculate the seat id', () => {
-       expect(BinaryBoardingPass.parse('FBFBBFFRLR').getSeatId()).to.equal(357);
-       expect(BinaryBoardingPass.parse('FFFFFFFLLL').getSeatId()).to.equal(0);
-       expect(BinaryBoardingPass.parse('BBBBBBBRRR').getSeatId()).to.equal(1023);
+        expect(BinaryBoardingPass.parse('FBFBBFFRLR').getSeatId()).to.equal(357);
+        expect(BinaryBoardingPass.parse('FFFFFFFLLL').getSeatId()).to.equal(0);
+        expect(BinaryBoardingPass.parse('BBBBBBBRRR').getSeatId()).to.equal(1023);
     });
-    it('should parse many boarding pass strings and calculate the seat id with the highest value', async () => {
+
+    describe('from file', () => {
         const inputFile = './test/exercises/advent-of-code-2020/day-5-binary-boarding-pass/input.txt';
-        const boardingPassStrs = await fs.readFileSync(inputFile, 'utf-8').split('\n').filter((s) => s.length > 0);
-        const maxSeatId = boardingPassStrs
-            .map((boardingPassStr) => BinaryBoardingPass.parse(boardingPassStr).getSeatId())
-            // .map((seatId) => console.log(seatId));
-            .reduce((a, c) => a > c ? a : c, 0);
-        console.log(maxSeatId);
-    })
+
+        it('should parse many boarding pass strings and calculate the seat id with the highest value', async () => {
+            const boardingPassStrs = await fs.readFileSync(inputFile, 'utf-8').split('\n').filter((s) => s.length > 0);
+            const maxSeatId = boardingPassStrs
+                .map((boardingPassStr) => BinaryBoardingPass.parse(boardingPassStr).getSeatId())
+                .reduce((a, c) => a > c ? a : c, 0);
+            console.log(maxSeatId);
+        })
+
+        it('should find the id of lone available seat, disregarding the front and back row', async () => {
+            // get seats for all recorded boarding passes
+            const allBoardingPassStrs = await fs.readFileSync(inputFile, 'utf-8').split('\n').filter((s) => s.length > 0);
+            const occupiedSeatIds = allBoardingPassStrs
+                .map((boardingPassStr) => BinaryBoardingPass.parse(boardingPassStr))
+                .map((s) => s.getSeatId());
+
+            const seatMinId = occupiedSeatIds.reduce((a, c) => a < c ? a : c, Number.MAX_SAFE_INTEGER);
+            const seatMaxId = occupiedSeatIds.reduce((a, c) => a > c ? a : c, 0);
+
+            // only one seat should be empty, meaning the number of occupied seats is one less than the INCLUSIVE range of occupied seat ids
+            expect(occupiedSeatIds.length).to.equal(seatMaxId - seatMinId + 1 - 1);
+
+            let emptySeatId = null;
+            for (let i = seatMinId; i <= seatMaxId; i++) {
+                if (occupiedSeatIds.find((id) => id === i) === undefined) {
+                    emptySeatId = i;
+                    break;
+                }
+            }
+
+            console.log(`seat ids from ${seatMinId} to ${seatMaxId}, empty seat: ${emptySeatId}`);
+        });
+    });
+
 });
