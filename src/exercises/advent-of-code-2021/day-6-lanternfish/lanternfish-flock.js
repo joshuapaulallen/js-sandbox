2598ca9012a58edb7795d2c82dd8d424e820feb7
Josh Allen <josh.allen@adcellerant.com>
2022-05-13 15:22:06 -1000

    advent of code 2021 - day 6b - a whole mess o' lanternfish, requiring a refactor of flock design

--- src/exercises/advent-of-code-2021/day-6-lanternfish/lanternfish-flock.js
+++ src/exercises/advent-of-code-2021/day-6-lanternfish/lanternfish-flock.js
@@ -1,56 +1,61 @@
-class Lanternish {
-
-    constructor(daysUntilSpawn = 0) {
-        this.daysUntilSpawn = Number(daysUntilSpawn);
-    }
-
-    tick() {
-        this.daysUntilSpawn--;
-    }
-
-    spawn() {
-        this.daysUntilSpawn = 6;
-
-        return new Lanternish(8);
-    }
-
-}
-
 class LanternfishFlock {
 
     /**
      * @constructor
-     * @param lanternfishes {Lanternish[]}
+     * @param countByDaysUntilSpawn {object} - A map whose keys are the # days left until spawning and whose values are
+     *                                         the number of lanternfish meeting that criteria.
      */
-    constructor(lanternfishes) {
-        this.lanternfishes = lanternfishes.splice(0);
+    constructor(countByDaysUntilSpawn) {
+        this.countByDaysUntilSpawn = { ...countByDaysUntilSpawn };
     }
 
     /**
      * @param lanternfishesStr {string}
      */
     static parse(lanternfishesStr) {
-        return new LanternfishFlock(lanternfishesStr.split(',').map(l => new Lanternish(l)));
+        const countByDaysUntilSpawn = {};
+        lanternfishesStr.split(',').forEach(daysUntilSpawn => {
+
+            const k = Object.keys(countByDaysUntilSpawn).find(k => k === daysUntilSpawn.toString());
+            if (k) {
+                countByDaysUntilSpawn[k]++;
+            } else {
+                countByDaysUntilSpawn[daysUntilSpawn.toString()] = 1;
+            }
+        })
+
+        return new LanternfishFlock(countByDaysUntilSpawn);
     }
 
     tick() {
-        let newLanternfishes = [];
-        this.lanternfishes.forEach(l => {
-            l.tick();
-            if (l.daysUntilSpawn < 0) {
-                newLanternfishes.push(l.spawn());
-            }
-        });
+        // define a new map of days until spawn -> fish count
+        let newCountByDaysUntilSpawn = {};
 
-        this.lanternfishes = this.lanternfishes.concat(newLanternfishes);
+        // for every index > 0, decrement the index
+        for (let i = 1; i <= 8; i++) {
+            if (this.countByDaysUntilSpawn[i.toString()]) {
+                newCountByDaysUntilSpawn[(i - 1).toString()] = this.countByDaysUntilSpawn[i.toString()];
+            }
+        }
+
+        // for index === 0, "spawn" a new set of fish at 8, and reset the timer at 6
+        if (this.countByDaysUntilSpawn["0"]) {
+            newCountByDaysUntilSpawn["8"] = this.countByDaysUntilSpawn["0"];
+            if (newCountByDaysUntilSpawn["6"]) {
+                // add to the existing bunch of fish at 6 days till spawn
+                newCountByDaysUntilSpawn["6"] += this.countByDaysUntilSpawn["0"];
+            } else {
+                newCountByDaysUntilSpawn["6"] = this.countByDaysUntilSpawn["0"];
+            }
+
+        }
+
+        // replace the map
+        this.countByDaysUntilSpawn = { ...newCountByDaysUntilSpawn };
     }
 
     count() {
-        return this.lanternfishes.length;
-    }
-
-    toString() {
-        return this.lanternfishes.map(l => l.daysUntilSpawn).join(',');
+        return Object.values(this.countByDaysUntilSpawn).reduce((a, c) => a + c, 0);
     }
 
 }
--- test/exercises/advent-of-code-2021/day-6-lanternfish/lanternfish-flock.spec.js
+++ test/exercises/advent-of-code-2021/day-6-lanternfish/lanternfish-flock.spec.js
@@ -1,7 +1,7 @@
 const fs = require('fs');
 const { LanternfishFlock } = require('../../../../src/exercises/advent-of-code-2021/day-6-lanternfish/lanternfish-flock');
 
-describe.only('Advent of Code 2021 - Day 6', () => {
+describe('Advent of Code 2021 - Day 6', () => {
     const inputFile = './test/exercises/advent-of-code-2021/day-6-lanternfish/input.txt';
 
     it('should count lanternfish of small sample after 18 days', () => {
@@ -10,7 +10,9 @@ describe.only('Advent of Code 2021 - Day 6', () => {
 
         const lanternfishFlock = LanternfishFlock.parse(lanternfishStr);
         for (let i = 0; i < days; i++) {
+            // console.log(lanternfishFlock.countByDaysUntilSpawn);
             lanternfishFlock.tick();
+
         }
 
         expect(lanternfishFlock.count()).to.equal(26);
@@ -28,11 +30,36 @@ describe.only('Advent of Code 2021 - Day 6', () => {
         expect(lanternfishFlock.count()).to.equal(5934);
     });
 
+    it('should count lanternfish of small sample after 256 days', () => {
+        const lanternfishStr = '3,4,3,1,2';
+        const days = 256;
+
+        const lanternfishFlock = LanternfishFlock.parse(lanternfishStr);
+        for (let i = 0; i < days; i++) {
+            lanternfishFlock.tick();
+        }
+
+        expect(lanternfishFlock.count()).to.equal(26984457539);
+    });
+
     it('should count lanternfish of large sample after 80 days', async () => {
         const lanternfishStr = await fs.readFileSync(inputFile, 'utf-8').trim();
         const start = new Date();
 
         const days = 80;
+        const lanternfishFlock = LanternfishFlock.parse(lanternfishStr);
+        for (let i = 0; i < days; i++) {
+            lanternfishFlock.tick();
+        }
+
+        console.log(`${lanternfishFlock.count()}: ${new Date() - start} ms`);
+    });
+
+    it('should count lanternfish of large sample after 256 days', async () => {
+        const lanternfishStr = await fs.readFileSync(inputFile, 'utf-8').trim();
+        const start = new Date();
+
+        const days = 256;
         const lanternfishFlock = LanternfishFlock.parse(lanternfishStr);
         for (let i = 0; i < days; i++) {
             lanternfishFlock.tick();
