9b69f6bec0fc1fee916ce720d74ec0b5e2f44475
Josh Allen <josh.allen@adcellerant.com>
2022-05-23 10:46:03 -1000

    advent of code 2021 - day 8b - digit descrambler part two (whew)

--- src/exercises/advent-of-code-2021/day-8-seven-segment-search/digit-descrambler.js
+++ src/exercises/advent-of-code-2021/day-8-seven-segment-search/digit-descrambler.js
@@ -22,7 +22,7 @@ class DigitDescrambler {
      * @param line {string}
      * @return {string} the digits that could be decoded, or '?' if a digit could not be decoded
      */
-    static decode(line) {
+    static decodeSimple(line) {
         const descrambler = this.parse(line);
 
         const decodeMap = {};
@@ -54,6 +54,108 @@ class DigitDescrambler {
         });
 
         return decodedDigits;
+    }
+
+    /**
+     * @param line {string}
+     * @return {string} the decoded digits
+     */
+    static decodeComplete(line) {
+        const descrambler = this.parse(line);
+
+        // create a map of clues by length
+        // 2: 1
+        // 3: 7
+        // 4: 4
+        // 5: 2, 3, 5
+        // 6: 0, 6, 9
+        // 7: 8
+        const cluesByLength = {};
+        descrambler.clues.forEach(clue => {
+            const normalizedClue = this.normalizeEncodedDigit(clue)
+            const i = Object.keys(cluesByLength).find(k => Number(k) === clue.length);
+            if (i) {
+                cluesByLength[i].push(normalizedClue);
+            } else {
+                cluesByLength[clue.length.toString()] = [normalizedClue];
+            }
+        });
+
+        // 1, 4, 7, and 8 have unique lengths
+        const decodeMap = {};
+        decodeMap["1"] = cluesByLength["2"][0];
+        decodeMap["4"] = cluesByLength["4"][0];
+        decodeMap["7"] = cluesByLength["3"][0];
+        decodeMap["8"] = cluesByLength["7"][0];
+
+        // create a map of letters to digit positions
+        const letterByPosition = {};
+
+        // the top-center position is the one in the 7 that is not included in the 1
+        letterByPosition["topCenter"] = decodeMap["7"].split('').find(c => !decodeMap["1"].includes(c));
+
+        // the center position is the one common to 2, 3, and 5 that is also common to 4
+        letterByPosition["center"] = this.findCommonChars(cluesByLength["5"]).split('')
+            .filter(c => decodeMap["4"].includes(c)).join('');
+
+        // the 0 is the clue of length 6 that does not have the center letter
+        decodeMap["0"] = cluesByLength["6"].find(clue => !clue.includes(letterByPosition["center"]));
+
+        // the 9 is the clue of length 6 that is not the 0 and also contains both letters in 1
+        decodeMap["9"] = cluesByLength["6"].find(
+            clue => clue !== decodeMap["0"]
+                && clue.includes(decodeMap["1"][0]) && clue.includes(decodeMap["1"][1])
+        );
+
+        // the 6 is the clue of length 6 that is neither the 0 nor 9
+        decodeMap["6"] = cluesByLength["6"].find(clue => clue !== decodeMap["0"] && clue !== decodeMap["9"]);
+
+        // the 3 is the clue of length 5 that contains both letters in 1
+        decodeMap["3"] = cluesByLength["5"].find(
+            clue => clue.includes(decodeMap["1"][0]) && clue.includes(decodeMap["1"][1])
+        );
+
+        // the 5 is the clue of length 5 that also contains all segments in 6
+        decodeMap["5"] = cluesByLength["5"].find(
+            clue => this.findCommonChars([clue, decodeMap["6"]]).length === 5
+        );
+
+        // the 2 is the clue of length 5 that is neither the 3 nor 5
+        decodeMap["2"] = cluesByLength["5"].find(clue => clue !== decodeMap["3"] && clue !== decodeMap["5"]);
+
+        // now that we have our complete decode map, translate to a digit
+        let decodedDigits = "";
+        descrambler.encodedDigits.forEach(encodedDigit => {
+            const normalizedEncodedDigit = this.normalizeEncodedDigit(encodedDigit);
+            const found = Object.keys(decodeMap).find(k => normalizedEncodedDigit === decodeMap[k]);
+            if (found) {
+                decodedDigits += found;
+            } else {
+                console.error(`did not find encoded digit '${encodedDigit}`);
+            }
+        });
+
+        // console.log(`${line} - ${decodedDigits}`);
+
+        return decodedDigits;
+    }
+
+    /**
+     * Finds the common characters in the given array of strings.
+     *
+     * @param strArray {string[]}
+     * @return {string}
+     */
+    static findCommonChars(strArray) {
+        if (strArray.length === 0 ) {
+            return '';
+        }
+        let commonChars = strArray[0];
+        for (let i = 1; i < strArray.length; i++) {
+            commonChars = commonChars.split('').filter(c => strArray[i].includes(c)).join('');
+        }
+
+        return commonChars;
     }
 
     static normalizeEncodedDigit(encodedDigit) {
--- test/exercises/advent-of-code-2021/day-8-seven-segment-search/digit-descrambler.spec.js
+++ test/exercises/advent-of-code-2021/day-8-seven-segment-search/digit-descrambler.spec.js
@@ -4,50 +4,104 @@ const { DigitDescrambler } = require('../../../../src/exercises/advent-of-code-2
 describe('Advent of Code 2021 - Day 8', () => {
     const inputFile = './test/exercises/advent-of-code-2021/day-8-seven-segment-search/input.txt';
 
-    it('should find the number of digits matching 1, 4, 7, or 8 in a small sample', () => {
-        const input = 'be cfbegad cbdgef fgaecd cgeb fdcge agebfd fecdb fabcd edb | fdgacbe cefdb cefbgd gcbe\n' +
-            'edbfga begcd cbg gc gcadebf fbgde acbgfd abcde gfcbed gfec | fcgedb cgb dgebacf gc\n' +
-            'fgaebd cg bdaec gdafb agbcfd gdcbef bgcad gfac gcb cdgabef | cg cg fdcagb cbg\n' +
-            'fbegcd cbd adcefb dageb afcb bc aefdc ecdab fgdeca fcdbega | efabcd cedba gadfec cb\n' +
-            'aecbfdg fbg gf bafeg dbefa fcge gcbea fcaegb dgceab fcbdga | gecf egdcabf bgf bfgea\n' +
-            'fgeab ca afcebg bdacfeg cfaedg gcfdb baec bfadeg bafgc acf | gebdcfa ecba ca fadegcb\n' +
-            'dbcfg fgd bdegcaf fgec aegbdf ecdfab fbedc dacgb gdcebf gf | cefg dcbef fcge gbcadfe\n' +
-            'bdfegc cbegaf gecbf dfcage bdacg ed bedf ced adcbefg gebcd | ed bcgafe cdgba cbgef\n' +
-            'egadfb cdbfeg cegd fecab cgb gbdefca cg fgcdab egfdb bfceg | gbdfcae bgc cg cgb\n' +
-            'gcafb gcf dcaebfg ecagb gf abcdeg gaef cafbge fdbac fegbdc | fgae cfgab fg bagce';
-        const inputLines = input.split('\n');
-        const decodedLines = inputLines.map(l => DigitDescrambler.decode(l));
+    describe('simple', () => {
+        it('should find the number of digits matching 1, 4, 7, or 8 in a small sample', () => {
+            const input = 'be cfbegad cbdgef fgaecd cgeb fdcge agebfd fecdb fabcd edb | fdgacbe cefdb cefbgd gcbe\n' +
+                'edbfga begcd cbg gc gcadebf fbgde acbgfd abcde gfcbed gfec | fcgedb cgb dgebacf gc\n' +
+                'fgaebd cg bdaec gdafb agbcfd gdcbef bgcad gfac gcb cdgabef | cg cg fdcagb cbg\n' +
+                'fbegcd cbd adcefb dageb afcb bc aefdc ecdab fgdeca fcdbega | efabcd cedba gadfec cb\n' +
+                'aecbfdg fbg gf bafeg dbefa fcge gcbea fcaegb dgceab fcbdga | gecf egdcabf bgf bfgea\n' +
+                'fgeab ca afcebg bdacfeg cfaedg gcfdb baec bfadeg bafgc acf | gebdcfa ecba ca fadegcb\n' +
+                'dbcfg fgd bdegcaf fgec aegbdf ecdfab fbedc dacgb gdcebf gf | cefg dcbef fcge gbcadfe\n' +
+                'bdfegc cbegaf gecbf dfcage bdacg ed bedf ced adcbefg gebcd | ed bcgafe cdgba cbgef\n' +
+                'egadfb cdbfeg cegd fecab cgb gbdefca cg fgcdab egfdb bfceg | gbdfcae bgc cg cgb\n' +
+                'gcafb gcf dcaebfg ecagb gf abcdeg gaef cafbge fdbac fegbdc | fgae cfgab fg bagce';
+            const inputLines = input.split('\n');
+            const decodedLines = inputLines.map(l => DigitDescrambler.decodeSimple(l));
 
-        const numDigits = decodedLines.map(line => {
-            let digitsInLine = 0;
-            for (let char of line) {
-                if (char !== '?') {
-                    digitsInLine++;
+            const numDigits = decodedLines.map(line => {
+                let digitsInLine = 0;
+                for (let char of line) {
+                    if (char !== '?') {
+                        digitsInLine++;
+                    }
                 }
-            }
-            return digitsInLine;
-        }).reduce((a, c) => a + c, 0);
+                return digitsInLine;
+            }).reduce((a, c) => a + c, 0);
 
-        expect(numDigits).to.equal(26);
+            expect(numDigits).to.equal(26);
+        });
+
+        it('should find the number of digits matching 1, 4, 7, or 8 in a large sample', async () => {
+            const input = await fs.readFileSync(inputFile, 'utf-8').trim();
+
+            const start = new Date();
+            const inputLines = input.split('\n');
+            const decodedLines = inputLines.map(l => DigitDescrambler.decodeSimple(l));
+
+            const numDigits = decodedLines.map(line => {
+                let digitsInLine = 0;
+                for (let char of line) {
+                    if (char !== '?') {
+                        digitsInLine++;
+                    }
+                }
+                return digitsInLine;
+            }).reduce((a, c) => a + c, 0);
+
+            console.log(`${numDigits}: ${new Date() - start} ms`);
+        });
     });
 
-    it('should find the number of digits matching 1, 4, 7, or 8 in a large sample', async () => {
-        const input = await fs.readFileSync(inputFile, 'utf-8').trim();
+    describe('complete', () => {
+        it('should decode and sum all digits in sample of one', () => {
+            const input = 'acedgfb cdfbe gcdfa fbcad dab cefabd cdfgeb eafb cagedb ab | cdfeb fcadb cdfeb cdbaf';
+            const inputLines = [input];
+            const decodedLines = inputLines.map(l => DigitDescrambler.decodeComplete(l));
 
-        const start = new Date();
-        const inputLines = input.split('\n');
-        const decodedLines = inputLines.map(l => DigitDescrambler.decode(l));
+            expect(decodedLines.length).to.equal(1);
+            expect(decodedLines[0]).to.equal('5353');
+        });
 
-        const numDigits = decodedLines.map(line => {
-            let digitsInLine = 0;
-            for (let char of line) {
-                if (char !== '?') {
-                    digitsInLine++;
-                }
-            }
-            return digitsInLine;
-        }).reduce((a, c) => a + c, 0);
+        it('should decode and sum all digits in a small sample', () => {
+            const input = 'be cfbegad cbdgef fgaecd cgeb fdcge agebfd fecdb fabcd edb | fdgacbe cefdb cefbgd gcbe\n' +
+                'edbfga begcd cbg gc gcadebf fbgde acbgfd abcde gfcbed gfec | fcgedb cgb dgebacf gc\n' +
+                'fgaebd cg bdaec gdafb agbcfd gdcbef bgcad gfac gcb cdgabef | cg cg fdcagb cbg\n' +
+                'fbegcd cbd adcefb dageb afcb bc aefdc ecdab fgdeca fcdbega | efabcd cedba gadfec cb\n' +
+                'aecbfdg fbg gf bafeg dbefa fcge gcbea fcaegb dgceab fcbdga | gecf egdcabf bgf bfgea\n' +
+                'fgeab ca afcebg bdacfeg cfaedg gcfdb baec bfadeg bafgc acf | gebdcfa ecba ca fadegcb\n' +
+                'dbcfg fgd bdegcaf fgec aegbdf ecdfab fbedc dacgb gdcebf gf | cefg dcbef fcge gbcadfe\n' +
+                'bdfegc cbegaf gecbf dfcage bdacg ed bedf ced adcbefg gebcd | ed bcgafe cdgba cbgef\n' +
+                'egadfb cdbfeg cegd fecab cgb gbdefca cg fgcdab egfdb bfceg | gbdfcae bgc cg cgb\n' +
+                'gcafb gcf dcaebfg ecagb gf abcdeg gaef cafbge fdbac fegbdc | fgae cfgab fg bagce';
+            const inputLines = input.split('\n');
+            const decodedLines = inputLines.map(l => DigitDescrambler.decodeComplete(l));
 
-        console.log(`${numDigits}: ${new Date() - start} ms`);
+            const sum = decodedLines.map(line => {
+                return Number(line)
+            }).reduce((a, c) => a + c, 0);
+
+            expect(sum).to.equal(61229);
+        });
+
+        it.only('should find the number of digits matching 1, 4, 7, or 8 in a large sample', async () => {
+            const input = await fs.readFileSync(inputFile, 'utf-8').trim();
+
+            const start = new Date();
+            const inputLines = input.split('\n');
+            const decodedLines = inputLines.map(l => DigitDescrambler.decodeComplete(l));
+
+            const sum = decodedLines.map(line => {
+                return Number(line)
+            }).reduce((a, c) => a + c, 0);
+
+            console.log(`${sum}: ${new Date() - start} ms`);
+        });
+    });
+
+    describe('utilities', () => {
+        it('should find common characters in a set of strings', () => {
+            expect(DigitDescrambler.findCommonChars(['abcd', 'bcde', 'bdeg'])).to.equal('bd');
+        });
     });
 });
